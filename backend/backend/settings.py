cd899aab3fd5593b15b3ae2c14c5f25bb3a36ed3
Abhay2004Kumar <abs220502@gmail.com>
2025-06-11 16:22:14 +0530

    updated settings for getenv

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,12 +34,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'DJANGO_SECRET_KEY'
+SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-default-secret")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "django-chat-rooms.onrender.com").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
 
 # Application definition
